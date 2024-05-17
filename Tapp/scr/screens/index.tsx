91741f842c28f4e0d06a312ca901c6bd2341488f
j4ogabriel <jgcduailibe15@gmail.com>
2024-05-17 11:59:40 -0300

    DEU CERT  PORRA
    hhh

---
+++ Tapp/scr/screens/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+export function SingnIn() {
+  return(
+    <View>
+      <Text>Tapp está em esenvolvimento, volte mais tarde :/</Text>
+    </View>
+  )
+}
